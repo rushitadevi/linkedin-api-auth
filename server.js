79f58e52740928129da6b9e63861b4f5ac658bca
Rushita <rushi.devi@gmail.com>
2019-10-25 12:35:04 +0200

    changes added

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ server.use("/profile",profileRouter)
 server.use("/post",postRouter)
 
 mongoose.connect("mongodb+srv://diegostriveschool:h6nxg5U9SDcsLA26@cluster0-3ar0p.azure.mongodb.net/test?retryWrites=true&w=majority", {
-  useNewUrlParser: true
+  useNewUrlParser: true,useUnifiedTopology: true
 }).then(
   server.listen(server.get('port'), () => {
       console.log("SERVER IS RUNNING ON " + server.get("port"))
